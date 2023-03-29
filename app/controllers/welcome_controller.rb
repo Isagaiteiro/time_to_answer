faad37d900e147be0add1841b133906ea54699c3
isagaiteiro137 <isouza@eslsistemas.com.br>
2023-03-29 14:53:31 -0300

    Autenticação básica HTTP

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+  #recurso de autenticação do navegador
+  http_basic_authenticate_with name: "isabela", password: "246809" 
+  
   def index
   end
 end
