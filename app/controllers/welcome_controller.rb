3442cab5c6ad66aeda96b4a7a6255edcdf0aa2c8
isagaiteiro137 <isouza@eslsistemas.com.br>
2023-03-30 14:51:34 -0300

    Criando os logins Admin e Profile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   #recurso de autenticação do navegador
-  http_basic_authenticate_with name: "isabela", password: "246809" 
+  #http_basic_authenticate_with name: "isabela", password: "246809" 
   
   def index
   end
