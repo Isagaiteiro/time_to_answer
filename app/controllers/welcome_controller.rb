7d17a1505b5841112cf1253466d248ab01bca480
isagaiteiro137 <isouza@eslsistemas.com.br>
2023-03-31 10:20:57 -0300

    Ajustando o sistema de layouts (Site)

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  #recurso de autenticação do navegador
-  #http_basic_authenticate_with name: "isabela", password: "246809" 
-  
-  def index
-  end
-end
